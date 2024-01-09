076d8ef8acee42a90c8069aa1bce743b8b883865
Abdelemjid Essaid <abdelemjidessaid@gmail.com>
2024-01-09 16:41:55 +0100

    task program is updated

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -11,27 +11,18 @@ def canUnlockAll(boxes):
         be visited using their contained indexes.
     """
 
-    if boxes is None or len(boxes) == 0:
-        return False
+    num_boxes = len(boxes)
+    unlocked_boxes = [False] * num_boxes
+    unlocked_boxes[0] = True
+    stack = [0]
+    
+    while stack:
+        curr_box = stack.pop()
+        
+        # Iterate through the keys in the current box
+        for key in boxes[curr_box]:
+            if key < num_boxes and not unlocked_boxes[key]:
+                unlocked_boxes[key] = True
+                stack.append(key)
 
-    visited = [0]
-    i = 0
-    # visite all boxes mentioned
-    while (i < len(boxes)):
-        box = boxes[i]
-        inc = True
-        for j in box:
-            if j in visited:
-                continue
-            else:
-                visited.append(j)
-                i = j
-                inc = False
-                break
-        if inc:
-            i += 1
-    # check if all boxes visited and return True or False
-    for i in range(len(boxes)):
-        if i not in visited:
-            return False
-    return True
+    return all(unlocked_boxes)
