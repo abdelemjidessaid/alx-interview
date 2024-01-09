0f54f9e58cef5424911004555cd3fee8ae25d876
Abdelemjid Essaid <abdelemjidessaid@gmail.com>
2024-01-09 16:11:28 +0100

    the task is solved

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+"""
+  Module that holds a python function of canUnlockAll
+"""
+
+
+def canUnlockAll(boxes):
+    """
+        Function that check a 2D Array if all arrays that holds can
+        be visited using their contained indexes.
+    """
+    visited = [0]
+    i = 0
+    # visite all boxes mentioned
+    while (i < len(boxes)):
+        box = boxes[i]
+        inc = True
+        for j in box:
+            if j in visited:
+                continue
+            else:
+                visited.append(j)
+                i = j
+                inc = False
+                break
+        if inc:
+            i += 1
+    # check if all boxes visited and return True or False
+    for i in range(len(boxes)):
+        if i not in visited:
+            return False
+    return True
