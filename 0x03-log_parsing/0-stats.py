10b44d89c5e684eba412e8c62142544ca0617906
Abdelemjid Essaid <abdelemjidessaid@gmail.com>
2024-01-25 12:24:52 +0100

    1st task is updated

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -25,8 +25,11 @@ def printResult():
         Function that prints the result of calculation after
         10 lines or after keyboard interruption
     """
+    # print the File size
     print(f'File size: {file_size}')
+    # sort status codes
     sorted_keys = sorted(codes.keys())
+    # print all keys and values of status codes except that has 0 in its value
     for key in sorted_keys:
         value = codes[key]
         if value:
@@ -35,22 +38,26 @@ def printResult():
 
 try:
     for line in sys.stdin:
+        # RegEx pattern to verify the input line instruction
         pattern = (
             r'(\d+\.\d+\.\d+\.\d+)' +
             r' - (\[\d+-\d+-\d+ \d+\:\d+\:\d+\.\d+\])' +
             r' (\"GET \/projects\/260 HTTP\/1.1\") (\d+) (\d+)')
+        # result of the RegEx
         result = re.findall(pattern, line)
+        # if RegEx result not None extract the Status code and File size
         if result:
+            # get just status and file size from input Line
             status = result[0][3]
             size = result[0][4]
             # increment the number of status code and file size
             codes[status] += 1
             file_size += int(size)
+        # increase the count var every iteration
         count += 1
+        # if count is equal to 10 print the result of summation
         if count == 10:
             printResult()
             count = 0
-except Exception:
-    pass
-finally:
+except KeyboardInterrupt:
     printResult()
