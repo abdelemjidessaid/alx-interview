994efe974905c88e1465b87fa113caba78dc10e4
Abdelemjid Essaid <abdelemjidessaid@gmail.com>
2024-01-29 15:47:48 +0100

    1st task is done

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+    Module of UTF-8 validation
+"""
+
+
+def validUTF8(data) -> bool:
+    """
+        Function that validates UTF-8 from an array of data
+    """
+    if type(data) is list and data:
+        if len(data) <= 64:
+            for value in data:
+                if not (type(value) is int and (value >= 0 and value <= 128)):
+                    return False
+            return True
+    return False
---
+++ 0x04-utf8_validation/README.md
@@ -0,0 +1,3 @@
+# 0x04-utf8_validation
+
+**This repository contains a program that validates the utf-8 array data**
