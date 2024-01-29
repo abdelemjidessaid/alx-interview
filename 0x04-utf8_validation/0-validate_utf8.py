730bf89419393638ce9857e61336fd2bd975f710
Abdelemjid Essaid <abdelemjidessaid@gmail.com>
2024-01-29 16:08:55 +0100

    1st task is updated

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -8,10 +8,29 @@ def validUTF8(data) -> bool:
     """
         Function that validates UTF-8 from an array of data
     """
+    left_bytes = 0
     if type(data) is list and data:
-        if len(data) <= 64:
-            for value in data:
-                if not (type(value) is int and (value >= 0 and value <= 128)):
-                    return False
-            return True
-    return False
+        for value in data:
+            if type(value) is int:
+                if value >> 6 == 0b10:
+                    if left_bytes == 0:
+                        return False
+                    left_bytes -= 1
+                else:
+                    if left_bytes > 0:
+                        return False
+                    if value >> 7 == 0b0:
+                        left_bytes = 0
+                    elif value >> 5 == 0b110:
+                        left_bytes = 1
+                    elif value >> 4 == 0b1110:
+                        left_bytes = 2
+                    elif value >> 3 == 0b11110:
+                        left_bytes = 3
+                    else:
+                        return False
+            else:
+                return False
+    else:
+        return False
+    return left_bytes == 0
